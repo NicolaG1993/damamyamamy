661f35bac58194d8219bf52c37c7b97f83ae22a2
Nicola Gaioni <nicog.designer@gmail.com>
2021-04-07 13:22:34 +0200

    checkout/address form done - with commerce.js

--- client/src/components/checkout-form/AddressForm.js
+++ client/src/components/checkout-form/AddressForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { commerce } from "../../lib/commerce";
 // import CustomTextField from "./CustomTextField";
 
@@ -16,6 +17,18 @@ export default function AddressForm({ checkoutToken }) {
         label: name,
     }));
     console.log("countries: ", countries);
+    const subdivisions = Object.entries(shippingSubdivisions).map(
+        ([code, name]) => ({
+            id: code,
+            label: name,
+        })
+    );
+    console.log("subdivisions: ", subdivisions);
+    const options = shippingOptions.map((sO) => ({
+        id: sO.id,
+        label: `${sO.description} - (${sO.price.formatted_with_symbol})`,
+    }));
+    console.log("options: ", options);
 
     const fetchShippingCountries = async (checkoutTokenId) => {
         const {
@@ -24,14 +37,52 @@ export default function AddressForm({ checkoutToken }) {
             checkoutTokenId
         );
 
-        console.log("fetched countries: ", countries);
+        // console.log("fetched countries: ", countries);
         setShippingCountries(countries);
         setShippingCountry(Object.keys(countries)[0]); //questo mi serve perché ricevo un oggetto invece di array (da commerce), e voglio solo i keys "countries" al suo interno // [0] é per avere il primo di questi
     };
 
+    const fetchSubdivisions = async (countryCode) => {
+        const { subdivisions } = await commerce.services.localeListSubdivisions(
+            countryCode
+        );
+
+        // console.log("fetched subdivisions: ", subdivisions);
+        setShippingSubdivisions(subdivisions);
+        setShippingSubdivision(Object.keys(subdivisions)[0]);
+    };
+
+    const fetchShippingOptions = async (
+        checkoutTokenId,
+        country,
+        region = null
+    ) => {
+        const options = await commerce.checkout.getShippingOptions(
+            checkoutTokenId,
+            { country, region }
+        );
+
+        // console.log("fetched options: ", options);
+        setShippingOptions(options);
+        setShippingOption(options[0].id);
+    }; //region = null se non cé né una // io ho una sola option al momento ma puo essere utile se si vogliono aggiungere in futuro -> good practice
+
     useEffect(() => {
         fetchShippingCountries(checkoutToken.id);
-    }, []);
+    }, []); //useEffect é come componentDidMount, viene eseguito solo al caricamento del component // perché [] alla fine
+
+    useEffect(() => {
+        if (shippingCountry) fetchSubdivisions(shippingCountry);
+    }, [shippingCountry]); //useEffect viene eseguito solo se cambia shippingCountry // perché [shippingCountry] alla fine // if exists
+
+    useEffect(() => {
+        if (shippingSubdivision)
+            fetchShippingOptions(
+                checkoutToken.id,
+                shippingCountry,
+                shippingSubdivision
+            );
+    }, [shippingSubdivision]);
 
     return (
         <div className={"address-form-box"}>
@@ -71,20 +122,41 @@ export default function AddressForm({ checkoutToken }) {
                     ))}
                 </select>
                 <br />
-                {/* <label>Choose a region *</label>
-                <select required name="region" id="region">
-                    <option value="lombardia">Lombardia</option>
-                    <option value="veneto">Veneto</option>
-                    <option value="trentino">Trentino - Alto Adige</option>
+                <label>Choose a region *</label>
+                <select
+                    required
+                    name="region"
+                    id="region"
+                    value={shippingSubdivision}
+                    onChange={(e) => setShippingSubdivision(e.target.value)}
+                >
+                    {subdivisions.map((subdivision) => (
+                        <option key={subdivision.id} value={subdivision.id}>
+                            {subdivision.label}
+                        </option>
+                    ))}
                 </select>
                 <br />
                 <label>Shipping options *</label>
-                <select required name="shipping" id="shipping">
-                    <option value="free">Free</option>
-                    <option value="express">Express</option>
+                <select
+                    required
+                    name="shipping"
+                    id="shipping"
+                    value={shippingOption}
+                    onChange={(e) => setShippingOption(e.target.value)}
+                >
+                    {options.map((option) => (
+                        <option key={option.id} value={option.id}>
+                            {option.label}
+                        </option>
+                    ))}
                 </select>
-                <br />
-                <input type="submit" value="Submit" /> */}
+                <div>
+                    <Link to="/cart">
+                        <button>Torna al carrello</button>
+                    </Link>
+                    <button type="submit">Prosegui</button>
+                </div>
             </form>
         </div>
     );
@@ -92,6 +164,6 @@ export default function AddressForm({ checkoutToken }) {
 
 /*
 
-guardare quali props di input sono reali invece di CustomTextField
+guardare quali props di input sono reali invece di CustomTextField (ex. "required"?)
 
 */
