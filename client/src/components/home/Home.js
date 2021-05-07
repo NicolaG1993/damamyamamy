42adc2562af244c77fa596251b1cbf26e03587e4
Nicola Gaioni <nicog.designer@gmail.com>
2021-05-07 15:21:07 +0200

    Checkout -> fixed a css bug in steps bar / Home -> added icons list

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -58,8 +58,35 @@ export default class Home extends Component {
                     />
                 </div>
 
-                <div className={"iconslist"}>
-                    Some icons here (affidabilitá, qualitá, etc..)
+                <div className="iconslist">
+                    <div className="iconslist-icon-box">
+                        <div className="iconslist-icon"></div>
+                        <h3>Affidabilità</h3>
+                        <p>
+                            Offriamo il migliore servizio, controllando ogni
+                            prodotto
+                        </p>
+                    </div>
+                    <div className="iconslist-icon-box">
+                        <div className="iconslist-icon"></div>
+                        <h3>Qualità</h3>
+                        <p>
+                            Da noi puoi aspettarti solo prodotti in ottime
+                            condizioni
+                        </p>
+                    </div>
+                    <div className="iconslist-icon-box">
+                        <div className="iconslist-icon"></div>
+                        <h3>Sicurezza</h3>
+                        <p>I prodotti per bambini sono sicuri al 100% </p>
+                    </div>
+                    <div className="iconslist-icon-box">
+                        <div className="iconslist-icon"></div>
+                        <h3>Rimborsi</h3>
+                        <p>
+                            In caso di prodotti rovinati é previsto il rimborso
+                        </p>
+                    </div>
                 </div>
             </div>
         );
