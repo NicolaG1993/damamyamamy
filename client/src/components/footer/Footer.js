a3456f0ff75175ce758ed8080f595576139bf7f6
Nicola Gaioni <nicog.designer@gmail.com>
2021-05-11 18:38:27 +0200

    adapted home and main for wide screen responsive

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -5,49 +5,51 @@ export default function Footer() {
 
     return (
         <div className={"footer-comp"}>
-            <div className={"footer"}>
-                <div className="website-footer">
-                    <h4>Links</h4>
-                    <p>
-                        <Link to={"/about"}>Chi siamo</Link>
-                    </p>
-                    <p>
-                        <Link to={"/shop"}>Prodotti</Link>
-                    </p>
-                    <p>Domande frequenti</p>
-                    <p>Vendi</p>
-                    <p>
-                        <Link to={"/contact"}>Contattaci / Assistenza</Link>
-                    </p>
-                </div>
+            <div className={"footer-wrap"}>
+                <div className={"footer"}>
+                    <div className="website-footer">
+                        <h4>Links</h4>
+                        <p>
+                            <Link to={"/about"}>Chi siamo</Link>
+                        </p>
+                        <p>
+                            <Link to={"/shop"}>Prodotti</Link>
+                        </p>
+                        <p>Domande frequenti</p>
+                        <p>Vendi</p>
+                        <p>
+                            <Link to={"/contact"}>Contattaci / Assistenza</Link>
+                        </p>
+                    </div>
 
-                <div className="legal-footer">
-                    <h4>Informazioni</h4>
-                    <p>Note legali</p>
-                    <p>Cookies policy</p>
-                    <p>Termini e condizioni</p>
-                    {/* <p>Metodi di pagamento / Spedizione</p> */}
-                </div>
+                    <div className="legal-footer">
+                        <h4>Informazioni</h4>
+                        <p>Note legali</p>
+                        <p>Cookies policy</p>
+                        <p>Termini e condizioni</p>
+                        {/* <p>Metodi di pagamento / Spedizione</p> */}
+                    </div>
 
-                <div className="contact-footer">
-                    <h4>Contatto</h4>
-                    <p>Vicolo Teatro, 4, 37010</p>
-                    <p>Cavaion, Verona, IT</p>
-                    <p>
-                        <a href="tel:+393479792644">(+39) 347 9792 644</a>
-                    </p>
-                    <p>
-                        <a href="mailto:damamyamamy@gmail.com">
-                            damamyamamy@gmail.com
-                        </a>
-                    </p>
-                </div>
+                    <div className="contact-footer">
+                        <h4>Contatto</h4>
+                        <p>Vicolo Teatro, 4, 37010</p>
+                        <p>Cavaion, Verona, IT</p>
+                        <p>
+                            <a href="tel:+393479792644">(+39) 347 9792 644</a>
+                        </p>
+                        <p>
+                            <a href="mailto:damamyamamy@gmail.com">
+                                damamyamamy@gmail.com
+                            </a>
+                        </p>
+                    </div>
 
-                <div className="social-footer">
-                    <a href="http://www.instagram.com/"></a>
-                    <a href="http://www.facebook.com/"></a>
-                    <a href="http://www.facebook.com/"></a>
-                    <a href="http://www.facebook.com/"></a>
+                    <div className="social-footer">
+                        <a href="http://www.instagram.com/"></a>
+                        <a href="http://www.facebook.com/"></a>
+                        <a href="http://www.facebook.com/"></a>
+                        <a href="http://www.facebook.com/"></a>
+                    </div>
                 </div>
             </div>
 
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -59,33 +59,36 @@ export default class Home extends Component {
                 </div>
 
                 <div className="iconslist">
-                    <div className="iconslist-icon-box">
-                        <div className="iconslist-icon"></div>
-                        <h3>Affidabilità</h3>
-                        <p>
-                            Offriamo il migliore servizio, controllando ogni
-                            prodotto
-                        </p>
-                    </div>
-                    <div className="iconslist-icon-box">
-                        <div className="iconslist-icon"></div>
-                        <h3>Qualità</h3>
-                        <p>
-                            Da noi puoi aspettarti solo articoli in ottime
-                            condizioni
-                        </p>
-                    </div>
-                    <div className="iconslist-icon-box">
-                        <div className="iconslist-icon"></div>
-                        <h3>Sicurezza</h3>
-                        <p>I prodotti per bambini sono sicuri al 100% </p>
-                    </div>
-                    <div className="iconslist-icon-box">
-                        <div className="iconslist-icon"></div>
-                        <h3>Rimborsi</h3>
-                        <p>
-                            In caso di prodotti rovinati é previsto il rimborso
-                        </p>
+                    <div className="iconslist-wrap">
+                        <div className="iconslist-icon-box">
+                            <div className="iconslist-icon"></div>
+                            <h3>Affidabilità</h3>
+                            <p>
+                                Offriamo il migliore servizio, controllando ogni
+                                prodotto
+                            </p>
+                        </div>
+                        <div className="iconslist-icon-box">
+                            <div className="iconslist-icon"></div>
+                            <h3>Qualità</h3>
+                            <p>
+                                Da noi puoi aspettarti solo articoli in ottime
+                                condizioni
+                            </p>
+                        </div>
+                        <div className="iconslist-icon-box">
+                            <div className="iconslist-icon"></div>
+                            <h3>Sicurezza</h3>
+                            <p>I prodotti per bambini sono sicuri al 100% </p>
+                        </div>
+                        <div className="iconslist-icon-box">
+                            <div className="iconslist-icon"></div>
+                            <h3>Rimborsi</h3>
+                            <p>
+                                In caso di prodotti rovinati é previsto il
+                                rimborso
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
