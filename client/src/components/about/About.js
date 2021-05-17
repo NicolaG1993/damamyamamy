743c15dda84278b8885e6651a7f6709f3f693ac2
Nicola Gaioni <nicog.designer@gmail.com>
2021-05-17 19:36:17 +0200

    About -> some design and new bio

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -14,26 +14,74 @@ export default function About(props) {
                 <div className="aboutus-box1-pic"></div>
                 <div className="aboutus-box1-text">
                     <p>
-                        {`You've asked for a JavaScript solution, so here's the shortest I can get it:`}
+                        Volevi vendere dei giocattoli mai usati di tuo figlio, o
+                        quel vestitino ancora nuovo che ormai non gli va piú
+                        bene?
+                    </p>
+                    <p>
+                        Oppure stai facendo acquisti per il tuo bambino ma
+                        vorresti anche risparmiare in sicurezza? Niente di piú
+                        facile...
                     </p>
                 </div>
             </div>
             <div className="aboutus-comp-div aboutus-box2">
                 <div className="aboutus-box2-text">
-                    <p>{`You can drop the semicolon at the end for one extra saved character, because JavaScript has "automatic semicolon insertion," a feature I normally despise and rail against, but in this specific use case it should be safe enough.`}</p>
+                    <p>
+                        Nasce Da Mamy a Mamy, il mercatino dell'usato dei
+                        genitori, per gli acquisti dei vostri bambini.
+                    </p>
+
+                    <p>
+                        Il nostro negozio ha aperto nel 2019 a Cavaion Veronese,
+                        ed ha subito attirato l'attenzione di genitori e
+                        famiglie, rivelandosi una nuovitá interessante nella
+                        provincia di Verona. Non solo perché si possono trovare
+                        prodotti di qualitá in ottime condizioni, ma anche
+                        perché i nostri clienti possono rivendere i loro
+                        articoli trammite di noi, lasciandoci prendere cura
+                        delle trattative e dei loro prodotti.
+                    </p>
+
+                    <p>
+                        Il negozio infatti nasce dall'idea di Sara Gagliardi,
+                        due volte mamma, che sa quindi riconoscere al meglio le
+                        esigenze delle proprie clienti, sempre disponibile a
+                        dare consiglio sul prossimo acquisto per i vostri
+                        bambini e nipoti; Inoltre offriamo un servizio di
+                        spedizione dei prodotti, disponibile in tutta Italia
+                    </p>
                 </div>
             </div>
             <div className="aboutus-comp-div aboutus-box3">
                 <div className="aboutus-box3-text">
-                    <p>{`It's important to note that this only works on browsers
-                        where JavaScript is enabled. Ideally, this would be
-                        better handled as an offline batch job (sed script on
-                        *nix, etc.) once a year, but if you want the JavaScript
-                        solution, I think that's as short as it gets. (Now I've
-                        gone and tempted fate.)`}</p>
+                    <p>
+                        I prodotti in vendita sono sicuri al 100% per i bambini,
+                        ogni articolo viene controllato ed approvato
+                        singolarmente prima di essere messo in vendita.
+                    </p>
+
+                    <p>
+                        Speriamo di vedervi presto in negozio / Venite a farci
+                        visita in negozio. Aperto dal lunedí al venerdi, dalle
+                        9:00 alle 16:00
+                    </p>
                 </div>
                 <div className="aboutus-box3-pic"></div>
             </div>
         </div>
     );
 }
+
+/*
+
+
+
+
+
+
+
+
+
+You've asked for a JavaScript solution, so here's the shortest I can get it:
+*/
