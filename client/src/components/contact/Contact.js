64740b9c8f812913e1acefd1b8a4ed4c372ed824
Nicola Gaioni <nicog.designer@gmail.com>
2021-05-18 21:30:15 +0200

    Contact -> design changes

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -19,85 +19,86 @@ export default function Contact() {
     };
 
     return (
-        <div className="contact-comp">
-            <h1>Contatta da Mamy a Mamy</h1>
+        <>
+            <div className="contact-comp">
+                <h1>Contatta da Mamy a Mamy</h1>
 
-            <form className="contact-form">
-                <div className="contact-form-col-left">
-                    <input
-                        type="text"
-                        placeholder="Nome*"
-                        name="contactname"
-                        id="contactname"
-                        onChange={(e) => handleForm(e)}
-                    />
+                <form className="contact-form">
+                    <div className="contact-form-col-left">
+                        <input
+                            type="text"
+                            placeholder="Nome*"
+                            name="contactname"
+                            id="contactname"
+                            onChange={(e) => handleForm(e)}
+                        />
+                    </div>
+
+                    <div className="contact-form-col-right">
+                        <input
+                            type="text"
+                            placeholder="Cognome*"
+                            name="contactlast"
+                            id="contactlast"
+                            onChange={(e) => handleForm(e)}
+                        />
+                    </div>
+
+                    <div className="contact-form-col-left">
+                        <input
+                            type="text"
+                            placeholder="Email*"
+                            name="email"
+                            id="email"
+                            onChange={(e) => handleForm(e)}
+                        />
+                    </div>
+
+                    <div className="contact-form-col-right">
+                        <input
+                            type="text"
+                            placeholder="Numero di telefono"
+                            name="phone"
+                            id="phone"
+                            onChange={(e) => handleForm(e)}
+                        />
+                    </div>
+
+                    <div className="contact-form-col-full">
+                        <textarea
+                            placeholder="Messaggio"
+                            name="message"
+                            id="message"
+                            onChange={(e) => handleForm(e)}
+                        />
+                    </div>
+
+                    <div className="contact-form-col-full">
+                        <button>Invia</button>
+                    </div>
+                </form>
+
+                <div className="contact-list">
+                    <h4>Contatto</h4>
+                    <p>Vicolo Teatro, 4, 37010</p>
+                    <p>Cavaion, Verona, IT</p>
+                    <p>
+                        <a href="tel:+393479792644">(+39) 347 9792 644</a>
+                    </p>
+                    <p>
+                        <a href="mailto:damamyamamy@gmail.com">
+                            damamyamamy@gmail.com
+                        </a>
+                    </p>
                 </div>
-
-                <div className="contact-form-col-right">
-                    <input
-                        type="text"
-                        placeholder="Cognome*"
-                        name="contactlast"
-                        id="contactlast"
-                        onChange={(e) => handleForm(e)}
-                    />
-                </div>
-
-                <div className="contact-form-col-left">
-                    <input
-                        type="text"
-                        placeholder="Email*"
-                        name="email"
-                        id="email"
-                        onChange={(e) => handleForm(e)}
-                    />
-                </div>
-
-                <div className="contact-form-col-right">
-                    <input
-                        type="text"
-                        placeholder="Numero di telefono"
-                        name="phone"
-                        id="phone"
-                        onChange={(e) => handleForm(e)}
-                    />
-                </div>
-
-                <div className="contact-form-col-full">
-                    <textarea
-                        placeholder="Messaggio"
-                        name="message"
-                        id="message"
-                        onChange={(e) => handleForm(e)}
-                    />
-                </div>
-
-                <div className="contact-form-col-full">
-                    <button>Invia</button>
-                </div>
-            </form>
-
-            <div className="contact-list">
-                <h4>Contatto</h4>
-                <p>Vicolo Teatro, 4, 37010</p>
-                <p>Cavaion, Verona, IT</p>
-                <p>
-                    <a href="tel:+393479792644">(+39) 347 9792 644</a>
-                </p>
-                <p>
-                    <a href="mailto:damamyamamy@gmail.com">
-                        damamyamamy@gmail.com
-                    </a>
-                </p>
             </div>
-
-            <div className="contact-map">
+            <div id="contact-map">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11178.56867680073!2d10.7683657!3d45.5374059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x49eb521541b62a58!2sDa%20Mamy%20a%20Mamy!5e0!3m2!1sit!2sde!4v1621274725301!5m2!1sit!2sde"
                     allowFullScreen=""
                     loading="lazy"
                 ></iframe>
             </div>
-        </div>
+        </>
     );
 }
