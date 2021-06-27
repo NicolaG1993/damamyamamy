fb3f9924ff7e07c5eaf8b0d227bd1a133d31b604
Nicola Gaioni <nicog.designer@gmail.com>
2021-06-27 11:18:20 +0200

    first deploy version

--- client/src/components/docs/Regolamento.js
+++ client/src/components/docs/Regolamento.js
@@ -13,56 +13,58 @@ export default function Regolamento() {
 
             <div className={"document-text-wrap"}>
                 <h3>
-                    Da Mamy A Mamy utilizza cookies per migliorare l’esperienza
-                    dell’utente, raccomandiamo di accettarne l’utilizzo per
-                    sfruttare a pieno la navigazione
+                    DA MAMY A MAMY SRL utilizza cookies per migliorare
+                    l’esperienza dell’utente, raccomandiamo di accettarne
+                    l’utilizzo per sfruttare a pieno la navigazione
                 </h3>
                 <p>
                     I Dati personali del Cliente al momento della creazione del
-                    conto cliente, sono destinati alla società DERNA GIPI SRL e
-                    si riserva inoltre il diritto di trasmettere le informazioni
-                    a terzi, previo accordo dato dal Cliente. Il Cliente che si
-                    iscrive sul Sito, s'impegna a fornire a DERNA GIPI SRL
-                    informazioni veritiere e corrette. Le informazioni raccolte
-                    sono necessarie alla gestione della relazione commerciale
-                    tra ogni Cliente e la società DERNA GIPI SRL. Nel quadro
-                    dell'emissione dell'ordine, le informazioni comunicate dal
-                    Cliente sono indispensabili per l'evasione e la consegna
-                    dell'ordine, nonché per la preparazione della fattura.
-                    L'assenza di queste informazioni, comporta come conseguenza
-                    l'annullamento dell'ordine. DERNA GIPI SRL s'impegna a
+                    conto cliente, sono destinati alla società DA MAMY A MAMY
+                    SRL e si riserva inoltre il diritto di trasmettere le
+                    informazioni a terzi, previo accordo dato dal Cliente. Il
+                    Cliente che si iscrive sul Sito, s&apos;impegna a fornire a
+                    DA MAMY A MAMY SRL informazioni veritiere e corrette. Le
+                    informazioni raccolte sono necessarie alla gestione della
+                    relazione commerciale tra ogni Cliente e la società DA MAMY
+                    A MAMY SRL. Nel quadro dell&apos;emissione dell&apos;ordine,
+                    le informazioni comunicate dal Cliente sono indispensabili
+                    per l&apos;evasione e la consegna dell&apos;ordine, nonché
+                    per la preparazione della fattura. L&apos;assenza di queste
+                    informazioni, comporta come conseguenza l&apos;annullamento
+                    dell&apos;ordine. DA MAMY A MAMY SRL s&apos;impegna a
                     prendere tutte le misure necessarie per assicurare la
                     protezione, la confidenzialità e la sicurezza dei dati
-                    personali (d'ora in poi «Dati Personali») che verranno
-                    trasmessi a DERNA GIPI SRL da ogni Cliente e ciò nel
+                    personali (d&apos;ora in poi «Dati Personali») che verranno
+                    trasmessi a DA MAMY A MAMY SRL da ogni Cliente e ciò nel
                     rispetto delle disposizioni di legge e applicabili. Il
-                    Cliente riconosce il diritto di DERNA GIPI SRL di registrare
-                    ogni dato relativo all'uso del conto, soprattutto ogni
-                    informazione relativa agli acquisti. Il Cliente riconosce di
-                    accettare che le operazioni ed il trattamento automatizzato
-                    dei dati Personali effettuato al momento della creazione del
-                    proprio conto cliente e dopo la creazione dello stesso,
-                    potranno essere effettuati da DERNA GIPI SRL e/o ogni altro
-                    partner contrattualmente legato direttamente o
-                    indirettamente a DERNA GIPI SRL. Le informazioni raccolte
-                    hanno come finalità di rendere possibile gli acquisti e di
-                    proporre promozioni, offerte commerciali e informazioni
-                    relative ai prodotti. Queste offerte e informazioni saranno
-                    inviate al Cliente a discrezione di DERNA GIPI SRL, previo
-                    accordo dato dalla cliente, DERNA GIPI SRL si riserva anche
-                    il diritto di trasmettere a terzi le informazioni raccolte.
+                    Cliente riconosce il diritto di DA MAMY A MAMY SRL di
+                    registrare ogni dato relativo all&apos;uso del conto,
+                    soprattutto ogni informazione relativa agli acquisti. Il
+                    Cliente riconosce di accettare che le operazioni ed il
+                    trattamento automatizzato dei dati Personali effettuato al
+                    momento della creazione del proprio conto cliente e dopo la
+                    creazione dello stesso, potranno essere effettuati da DA
+                    MAMY A MAMY SRL e/o ogni altro partner contrattualmente
+                    legato direttamente o indirettamente a DA MAMY A MAMY SRL.
+                    Le informazioni raccolte hanno come finalità di rendere
+                    possibile gli acquisti e di proporre promozioni, offerte
+                    commerciali e informazioni relative ai prodotti. Queste
+                    offerte e informazioni saranno inviate al Cliente a
+                    discrezione di DA MAMY A MAMY SRL, previo accordo dato dalla
+                    cliente, DA MAMY A MAMY SRL si riserva anche il diritto di
+                    trasmettere a terzi le informazioni raccolte.
                 </p>
                 <div className="end-paragraph"></div>
 
                 <h3>ACCESSO, MODIFICA, RITIRO DEI DATI PERSONALI</h3>
                 <p>
-                    In conformità alla legge sull'informatica, i file e le
+                    In conformità alla legge sull&apos;informatica, i file e le
                     libertà n°78-17 del 6 gennaio 1978, il Cliente dispone del
                     diritto di accesso, modifica e rettifica, opposizione e
-                    soppressione dei dati che lo riguardano presso DERNA GIPI
-                    SRL e ciò dopo verifica condotta DERNA GIPI SRL - se
-                    ritenuto opportuno - dell'identità del Cliente tramite
-                    documento d'identità valido.
+                    soppressione dei dati che lo riguardano presso DA MAMY A
+                    MAMY SRL e ciò dopo verifica condotta DA MAMY A MAMY SRL -
+                    se ritenuto opportuno - dell&apos;identità del Cliente
+                    tramite documento d&apos;identità valido.
                 </p>
                 <p>
                     Il cliente ha inoltre facoltà di opporsi al trattamento dei
@@ -70,17 +72,17 @@ export default function Regolamento() {
                 </p>
                 <p>
                     • A questo fine, basta che il Cliente ne faccia domanda per
-                    iscritto all'indirizzo seguente:
+                    iscritto all&apos;indirizzo seguente:
                 </p>
                 <address>
-                    DERNA GIPI SRL <br />
-                    Vicolo Parrocchia, 4/6 37016
+                    DA MAMY A MAMY SRL <br />
+                    Vicolo Teatro, 4 37010
                 </address>
-                <address>MALCESINE (VR)</address>
-                <address>E-mail: customerservice@derna.it </address>
-                <address>Tel: +39-(0)45-7400967 </address>
+                <address>CAVAION VERONESE (VR)</address>
+                <address>E-mail: customerservice@damamyamamy.it </address>
+                <address>Tel: +39-347-9792-644 </address>
                 <p>
-                    DERNA GIPI SRL propone al Cliente di ricevere via e-mail
+                    DA MAMY A MAMY SRL propone al Cliente di ricevere via e-mail
                     (posta elettronica) informazioni che riguardano le novità,
                     le offerte promozionali, le offerte di svendita, le vendite
                     esclusive, i propri buoni affari.
@@ -101,9 +103,9 @@ export default function Regolamento() {
 
                 <h3>COS´É UN COOKIE ?</h3>
                 <p>
-                    Un cookie è un'informazione deposta nel vostro hard disk dal
-                    server del Sito che visitate. Esso contiene diversi dati,
-                    tra cui:
+                    Un cookie è un&apos;informazione deposta nel vostro hard
+                    disk dal server del Sito che visitate. Esso contiene diversi
+                    dati, tra cui:
                 </p>
                 <ul>
                     <li>il nome del server che lo ha deposto;</li>
@@ -127,25 +129,25 @@ export default function Regolamento() {
                     migliorare la navigazione, di effettuare delle statistiche,
                     di conservare delle informazioni relative alla costituzione
                     del carrello e della sua validazione, nonché di conservare
-                    gli identificativi di collegamento al conto fedeltà cliente.{" "}
+                    gli identificativi di collegamento al conto fedeltà cliente.
                 </p>
                 <p>
                     Altri cookies aiutano a capire il comportamento degli utenti
-                    per rendere l'esperienza più pertinente e adeguata ai centri
-                    d'interesse (Esempio: raccomandazioni personalizzate sugli
-                    articoli).
+                    per rendere l&apos;esperienza più pertinente e adeguata ai
+                    centri d&apos;interesse (Esempio: raccomandazioni
+                    personalizzate sugli articoli).
                 </p>
                 <p>
                     I cookies possono inoltre essere usati in maniera
                     personalizzata o anonima per mostrare delle pubblicità o
-                    permettere l'invio di e-mail secondo le abitudini di
+                    permettere l&apos;invio di e-mail secondo le abitudini di
                     navigazione. Inoltre, potrebbero essere attivati dei cookies
                     specialmente concepiti per «reti sociali». Essi permettono
-                    all'utente di interagire tramite le reti sociali (Esempio:
-                    funzione condividi). Quando una pagina contiene questo
-                    comando, si stabilisce un collegamento diretto con la rete
-                    sociale selezionata tramite cui possono essere trasmesse
-                    alcune informazioni.
+                    all&apos;utente di interagire tramite le reti sociali
+                    (Esempio: funzione condividi). Quando una pagina contiene
+                    questo comando, si stabilisce un collegamento diretto con la
+                    rete sociale selezionata tramite cui possono essere
+                    trasmesse alcune informazioni.
                 </p>
                 <div className="end-paragraph"></div>
 
@@ -155,7 +157,7 @@ export default function Regolamento() {
                 </h3>
                 <p>
                     Le informazioni immagazzinate dai cookies dei nostri Siti
-                    sono destinate a DERNA GIPI SRL.
+                    sono destinate a DA MAMY A MAMY SRL.
                 </p>
                 <div className="end-paragraph"></div>
 
@@ -165,12 +167,13 @@ export default function Regolamento() {
                     funzione «opzioni»/«strumenti» del vostro browser.
                 </p>
                 <p>
-                    La soppressione dei cookies può interrompere l'iter
-                    d'acquisto, scollegare il conto fedeltà e far perdere le
-                    funzionalità create per rendere più semplice la navigazione
-                    e l'acquisto. In questi casi, ma non solo, DERNA GIPI SRL
-                    non potrà essere ritenuta responsabile del malfunzionamento
-                    del Sito o della diminuita efficacia delle sue funzionalità.
+                    La soppressione dei cookies può interrompere l&apos;iter
+                    d&apos;acquisto, scollegare il conto fedeltà e far perdere
+                    le funzionalità create per rendere più semplice la
+                    navigazione e l&apos;acquisto. In questi casi, ma non solo,
+                    DA MAMY A MAMY SRL non potrà essere ritenuta responsabile
+                    del malfunzionamento del Sito o della diminuita efficacia
+                    delle sue funzionalità.
                 </p>
                 <div className="end-paragraph"></div>
 
@@ -179,7 +182,7 @@ export default function Regolamento() {
                     In questo caso, la totalità dei Siti visitata è messa in
                     questione. Questa regolazione può essere fatta in qualsiasi
                     momento e da qualsiasi browser internet, basta andare sul
-                    menù d'aiuto del navigatore.
+                    menù d&apos;aiuto del navigatore.
                 </p>
                 <p>
                     <Link>LEGGI LA COOKIE POLICY COMPLETA QUI</Link>
