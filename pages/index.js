d89b1e037fcf446081958b6538627b6b2a380ae7
Nicola Gaioni <nicog.designer@gmail.com>
2022-01-05 09:48:24 +0100

    trying to fix deploy errors

--- pages/index.js
+++ pages/index.js
@@ -124,14 +124,14 @@ export default function Home({ catNewItems, cat1, cat2 }) {
     );
 }
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
     // const { data } = await axios.get(
     //     `http://localhost:3000/api/home-categories`
     // );
-    // const { data } = await axios.get(`/api/home-categories`);
-    const { data } = await axios.get(
-        `https://damamyamamy.com/api/home-categories`
-    );
+    const { data } = await axios.get(`/api/home-categories`);
+    // const { data } = await axios.get(
+    //     `https://damamyamamy.com/api/home-categories`
+    // );
 
     return {
         props: {
