037bb62faeee968d5a798311c28988e6a3e62f35
Nicola Gaioni <nicog.designer@gmail.com>
2023-02-23 19:28:27 +0100

    added some db queries

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,5 @@
 import data from "@/utils/db/data";
 
 export default function handler(req, res) {
-    res.status(200).send(data);
+    // res.status(200).send(data);
 }
--- utils/db/db.js
+++ utils/db/db.js
@@ -15,3 +15,112 @@ if (!db) {
 }
 
 module.exports.db;
+
+/* NEW */
+module.exports.newItem = (
+    name,
+    pics,
+    price,
+    count_in_stock,
+    slug,
+    description,
+    info,
+    condition
+) => {
+    const myQuery = `INSERT INTO item 
+    (name, pics, price, count_in_stock, slug, description, info, condition) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+    RETURNING *`;
+    const keys = [
+        name,
+        pics,
+        price,
+        count_in_stock,
+        slug,
+        description,
+        info,
+        condition,
+    ];
+    return db.query(myQuery, keys);
+};
+module.exports.newUser = (first_name, last_name, email, psw) => {
+    const myQuery = `INSERT INTO user 
+    (first_name, last_name, email, psw) 
+    VALUES ($1, $2, $3, $4) 
+    RETURNING *`;
+    const keys = [first_name, last_name, email, psw];
+    return db.query(myQuery, keys);
+};
+module.exports.newOrder = ({
+    user_id,
+    shipping_address,
+    payment_method,
+    payment_result,
+    items_price,
+    shipping_price,
+    tax_price,
+    total_price,
+    is_paid,
+    is_delivered,
+    paid_at,
+    delivered_at,
+}) => {
+    const myQuery = `INSERT INTO order
+    (user_id, shipping_address, payment_method, payment_result, items_price, shipping_price, tax_price, total_price, is_paid, is_delivered, paid_at, delivered_at)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+    RETURNING *`;
+    const keys = [
+        user_id,
+        shipping_address,
+        payment_method,
+        payment_result,
+        items_price,
+        shipping_price,
+        tax_price,
+        total_price,
+        is_paid,
+        is_delivered,
+        paid_at,
+        delivered_at,
+    ];
+    return db.query(myQuery, keys);
+};
+module.exports.newTag = (name) => {
+    const myQuery = `INSERT INTO tag 
+    (name) 
+    VALUES ($1) 
+    RETURNING *`;
+    const keys = [name];
+    return db.query(myQuery, keys);
+};
+module.exports.newCategory = (name) => {
+    const myQuery = `INSERT INTO category 
+    (name) 
+    VALUES ($1) 
+    RETURNING *`;
+    const keys = [name];
+    return db.query(myQuery, keys);
+};
+module.exports.newBrand = (name) => {
+    const myQuery = `INSERT INTO brand 
+    (name) 
+    VALUES ($1) 
+    RETURNING *`;
+    const keys = [name];
+    return db.query(myQuery, keys);
+};
+
+/* EDIT */
+
+/* GET */
+module.exports.getElementByID = (table, id) => {
+    const myQuery = `SELECT * FROM ${table} WHERE id = $1`;
+    const key = [id];
+    return db.query(myQuery, key);
+};
+
+/* GET ALL */
+module.exports.getAllItems = () => {
+    const myQuery = `SELECT * FROM item WHERE count_in_stock >= 1`;
+    return db.query(myQuery);
+};
