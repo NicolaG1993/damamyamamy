36c39e828bc0153be54dd9d9e12b61c8e2e613fd
Nicola Gaioni <nicog.designer@gmail.com>
2023-03-08 13:26:41 +0100

    fixing bugs for production deployment

---
+++ pages/documenti/temini.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Termini() {
+    return (
+        <>
+            <Head>
+                <title>Termini e condizioni • Da Mamy a Mamy</title>
+                <meta
+                    property="og:title"
+                    content="Termini e condizioni • Da Mamy a Mamy"
+                />
+                <meta property="og:type" content="website" />
+            </Head>
+
+            <main>
+                <section className="page">
+                    <h1>Termini e condizioni</h1>
+                </section>
+            </main>
+        </>
+    );
+}
