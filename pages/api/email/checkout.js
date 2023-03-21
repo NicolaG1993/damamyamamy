3d68779d35c02da37c2c5c1aa5db5a09faefa70f
Nicola Gaioni <nicog.designer@gmail.com>
2023-03-21 22:18:58 +0100

    email for shop on checkout

--- pages/api/email/checkout.js
+++ pages/api/email/checkout.js
@@ -16,7 +16,9 @@ export default async function handler(req, res) {
                 sender,
                 domain,
             } = body;
-            let emailData = {
+
+            /* USER EMAIL */
+            let userEmailData = {
                 first: first,
                 last: last,
                 recipient: email,
@@ -47,8 +49,43 @@ export default async function handler(req, res) {
                         <p>Grazie per averci scelto!</p>`,
                 // phone: phone,
             };
-            let response = await sendEmail(emailData);
-            res.send(response);
+
+            /* SHOP EMAIL */
+            let shopEmailData = {
+                first: first,
+                last: last,
+                recipient: "damamyamamy@gmail.com",
+                source: "Da Mamy a Mamy <damamyamamy@gmail.com>",
+                subject: "Hai ricevuto un ordine da " + first + "!",
+                message: `<p>Utente: ${first} ${last}</p>
+                        <p>Email: ${email}</p>
+                        <p>ID ordine: ${order_uuid}</p>
+                        <div>
+                            <h3>Articoli acquistati:</h3>
+                            ${order_items.map((el) => {
+                                console.log("🔍🔍🔍 ", el);
+                                return `<div
+                                        key=${"articolo: " + el.id}
+                                        style="display: flex; align-items: center;"
+                                    >
+                                        <p>${el.name}</p>
+                                        <p style="padding: 0 20px;">€ ${
+                                            el.price
+                                        }</p>
+                                        <p>x${el.quantity}</p>
+                                    </div>`;
+                            })}
+                            <p>Importo totale: € ${total_price}</p>
+                        </div>
+                        <a href="${
+                            process.env.DOMAIN
+                        }/ordine/${order_uuid}">Vedi l'ordine sul sito</a>`,
+                // phone: phone,
+            };
+
+            let userEmailRes = await sendEmail(userEmailData);
+            let shopEmailRes = await sendEmail(shopEmailData);
+            res.send({ userEmailRes, shopEmailRes });
         } catch (err) {
             console.log("🐞 ERROR: ", err);
             res.status(500).json({ message: "Error occured." });
