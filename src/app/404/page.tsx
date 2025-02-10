61eb626743f76e5c3f8dd570885d5e4275f0e0da
NicolaG1993 <nicog.designer@gmail.com>
2025-02-10 11:28:14 +0100

    404 page

---
+++ src/app/404/page.tsx
@@ -0,0 +1,12 @@
+export default function ForbiddenPage() {
+    return (
+        <main>
+            <section>
+                <div>
+                    <h1>404</h1>
+                    <p>Non troviamo la pagina che stai cercando.</p>
+                </div>
+            </section>
+        </main>
+    );
+}
