5233afb8cbd44d38b958abf545ea8063be524d10
NicolaG1993 <nicog.designer@gmail.com>
2025-04-24 19:20:13 +0200

    removed loose comment

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -47,14 +47,6 @@ export default function Shop() {
         try {
             const data = await getShopPage(currentFilters);
             setItems(data.items);
-            console.log({
-                data,
-                "Number(data.total)": Number(data.total),
-                "currentFilters.countPerPage": currentFilters.countPerPage,
-                "Total Pages": Math.ceil(
-                    Number(data.total) / currentFilters.countPerPage
-                ),
-            });
             setTotalPages(
                 Math.ceil(Number(data.total) / currentFilters.countPerPage)
             );
